e32172aea8649111f8eb8277fe7bfabc6748e2c4
Natalya Myunster <natalyamyunster@yahoo.com>
2021-03-31 22:43:42 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,77 @@
+// task 1
+
+alert(60*60*24);
+
+// task 2
+
+alert(60*60*24*30);
+
+// task3
+
+alert(60*60*24*30*12);
+
+// task4
+
+alert(60*24);
+
+// task5
+
+alert(60*24*30*12);
+
+// task6
+
+alert(1024*1024);
+
+// task7
+
+alert(1024*1024*1024);
+
+// task8
+
+alert(1024*1024*1024*10);
+
+// task9
+
+alert(1024*1024*1024*1024);
+
+// task10
+
+alert(1024*1024*1024);
+
+// task11
+
+let r = 4;
+let s = 3.14  * r * r;
+alert(s);
+
+// task12
+
+let a = 4;
+let b = a * 4;
+alert(b);
+
+// task13
+
+let c = 2;
+let d = 5;
+let e = c * d;
+alert(e);
+
+// task14
+
+let f = 2;
+let g = 6;
+let p = f + g;
+alert(p);
+
+// task15
+
+let tc = 40;
+let fa = (tc * 9 / 5) + 32;
+alert(fa);
+
+// task16
+
+let tf = 100;
+let cl = (tf - 32) * 5 / 9;
+alert(cl);
